de7f581a4c37994f2282948b142b75a2071ac8af
root <root@ip-172-31-16-94.ap-northeast-1.compute.internal>
2019-03-12 01:11:36 +0000

    install.sh修正

--- kitchensink/extensions/install.sh
+++ kitchensink/extensions/install.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
+#!/bin/sh
 injected_dir=$1
 source /usr/local/s2i/install-common.sh
-install_deployments ${injected_dir}/injected-deployments.war
-install_modules ${injected_dir}/modules
+install_modules ${injected_dir}/modules/
 configure_drivers ${injected_dir}/drivers.env
